d529936966de2bd10f65f165ee26f48a34955edc
nandu961101 <nandu961101@gmail.com>
2026-08-03 18:57:41 +0530

    Fix multiply test case to expect correct result
    changes made in the multiply operation result

--- test_calculator.py
+++ test_calculator.py
@@ -14,7 +14,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(4, 3), 2)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
